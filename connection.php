04d1549fb422096f06d5a05514825f5066bf9eaa
wanzbi3 <wanzdhy@gmail.com>
2022-10-29 17:41:50 +0700

    commit pertama

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+    $host ="localhost";
+    $user ="root";
+    $pass ="";
+    $db ="dbpaswa1";
+
+    $mysqli = mysqli_connect(
+        $host,
+        $user,
+        $pass,
+        $db,
+    )
+?>
